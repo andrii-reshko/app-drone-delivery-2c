d729fd60422e9e88ad2b522155491e0ca85e5a03
Andrii Reshko <reshko.andrii@student.uzhnu.edu.ua>
2024-10-20 00:09:02 +0300

    upd: bind hooks to views

--- src/pages/send/send-order-view.tsx
+++ src/pages/send/send-order-view.tsx
@@ -1,9 +1,13 @@
 import { Link, useParams } from "react-router-dom";
 import { trimUuid } from "@/helpers";
 import PageMeta from "@/components/common/page-meta.tsx";
-import SplitLayout from "@/components/layout/split-layout.tsx";
 import Card from "@/components/common/card.tsx";
 import Toolbar from "@/components/common/toolbar.tsx";
+import { useEffect, useState } from "react";
+import {
+  useCreateOutgoingOrder,
+  useShowOutgoingOrder,
+} from "@/hooks/outgoing-orders.ts";
 
 type RouteParams = {
   uuid?: string;
@@ -14,22 +18,46 @@ const SendOrderView = () => {
 
   const shortUuid = uuid ? trimUuid(uuid as string) : "Add";
 
+  const {
+    instance: loadedInstance,
+    loading,
+    fetchItem,
+  } = useShowOutgoingOrder();
+
+  const { instance: newInstance, createInstance } = useCreateOutgoingOrder();
+
+  const [instance, setInstance] = useState(loadedInstance || newInstance);
+
+  useEffect(() => {
+    if (uuid) fetchItem(uuid);
+    else createInstance();
+  }, [uuid]);
+
+  useEffect(() => {
+    setInstance(loadedInstance);
+  }, [loadedInstance]);
+
+  useEffect(() => {
+    setInstance(newInstance);
+  }, [newInstance]);
+
   return (
     <>
       <PageMeta title={`${shortUuid} – Outgoing`} />
-      <SplitLayout>
-        <Card
-          className={"flex-fill"}
-          header={
-            <Toolbar title={"Outgoing Order"} subtitle={`# ${shortUuid}`} />
-          }
-          footer={
-            <Link className={"btn btn-secondary"} to={"/send"}>
-              Back
-            </Link>
-          }
-        ></Card>
-      </SplitLayout>
+      <Card
+        className={"flex-fill"}
+        header={
+          <Toolbar title={"Outgoing Order"} subtitle={`# ${shortUuid}`} />
+        }
+        footer={
+          <Link className={"btn btn-secondary"} to={"/send"}>
+            Back
+          </Link>
+        }
+      >
+        {loading && <p>Loading...</p>}
+        {instance && <pre>{JSON.stringify(instance, null, 2)}</pre>}
+      </Card>
     </>
   );
 };
